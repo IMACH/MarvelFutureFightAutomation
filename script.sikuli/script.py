0c562713770aa1136d30dda97040e5ac5df0ac62
IMACH <mr.kevinmach@gmail.com>
2016-05-08 22:47:02 -0700

    Mission: Next Stage
    Added the ability to move onto the next stage after all biometrics for
    current stage have been acquired. 0/5

--- script.sikuli/script.py
+++ script.sikuli/script.py
@@ -1,15 +1,23 @@
 missionRunning = False 
 click("1462760519675.png")
-click("1462765514122.png")
+click("1462771079204.png")
 
 
-while(not exists(Pattern("1462765029872.png").similar(0.80))):
-    click("1462760893884.png")
-    while(not exists(Pattern("1462763144399.png").targetOffset(1,-5))):
-        missionRunning = True
-        exists(Pattern("1462764604011.png").targetOffset(-2,-2))
-        missionRunning = False
-    click(Pattern("1462764604011.png").targetOffset(-2,-2))
+for x in range(20):
+    while(not exists(Pattern("1462765029872.png").similar(0.80))):
+        click("1462760893884.png")
+        while(not exists(Pattern("1462763144399.png").targetOffset(1,-5))):
+            missionRunning = True 
+            exists(Pattern("1462764604011.png").targetOffset(-2,-2))
+            missionRunning = False
+        click(Pattern("1462764604011.png").targetOffset(-2,-2))
+        if exists(Pattern("1462765029872.png").similar(0.80)):
+            click(Pattern("1462769658093.png").exact())
+    x += 1
+            
+
+    
+    
 
 
 
